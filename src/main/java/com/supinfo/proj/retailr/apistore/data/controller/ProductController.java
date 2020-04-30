cacef36355c50160cf352605951d5176331128c1
eweber_42 <eweber@korben.com>
2020-04-30 23:33:00 +0200

    extended product rest patterns

--- src/main/java/com/supinfo/proj/retailr/apistore/data/controller/ProductController.java
+++ src/main/java/com/supinfo/proj/retailr/apistore/data/controller/ProductController.java
@@ -2,13 +2,18 @@ package com.supinfo.proj.retailr.apistore.data.controller;
 
 import com.supinfo.proj.retailr.apistore.data.entity.Product;
 import com.supinfo.proj.retailr.apistore.data.repository.ProductRepository;
+import javassist.NotFoundException;
+import org.aspectj.weaver.ast.Not;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("/products")
+@RequestMapping
 public class ProductController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
@@ -16,14 +21,54 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping
-    public Iterable<Product> getProductsByEan(@RequestParam(name = "ean", required = false) String ean){
-        if (ean == null){
+    @GetMapping("/products")
+    public Iterable<Product> getProducts(){
             logger.info("/GET on /products with no ean");
             return this.productRepository.findAll();
+    }
+
+    @GetMapping("/products/{ean}")
+    public Product getProductByEan(@PathVariable String ean) throws NotFoundException{
+        logger.info("GET on /products/{ean} with param : " + ean);
+        return this.productRepository.findProductByEan(ean)
+                .orElseThrow(() -> new NotFoundException("product with ean " + ean + " does not exist"));
+    }
+
+    @GetMapping("/products/department/{department}")
+    public Iterable<Product> getProductByDepartment(@PathVariable String department){
+        logger.info("GET on /products/department/{department} with param : " + department);
+        return this.productRepository.findProductByDepartment(department);
+    }
+
+    @GetMapping("/products/family/{family}")
+    public Iterable<Product> getProductByFamily(@PathVariable String family){
+        logger.info("GET on /products/family/{family} with param : " + family);
+        return this.productRepository.findProductByFamily(family);
+    }
+
+    @GetMapping("/products/subfamily/{subfamily}")
+    public Iterable<Product> getProductBySubfamily(@PathVariable String subfamily){
+        logger.info("GET on /products/subfamily/{subfamily} with param : " + subfamily);
+        return this.productRepository.findProductBySubfamily(subfamily);
+    }
+
+    @PostMapping("/products/update/{ean}")
+    public String editProduct(@PathVariable String ean, @Valid @RequestBody Product product, BindingResult result) throws NotFoundException{
+        logger.info("POST on /products/update/{ean} with product of ean : " + product.getEan());
+            this.productRepository.findProductByEan(ean)
+            .orElseThrow(() -> new NotFoundException("Product with ean " + ean + " does not exist"));
+            productRepository.save(product);
+            return "{\"Product successfully edited\":1}";
+    }
+
+    @PostMapping("/products/add")
+    public String createProduct(@Valid @RequestBody Product product, BindingResult result){
+        logger.info("POST on /products/add with product of param = " + product.getEan());
+        if (result.hasErrors()){
+            return "{\"Invalid format\":1}";
         } else {
-            logger.info("GET on /products with param : ean = " + ean);
-            return this.productRepository.findProductByEan(ean);
+            productRepository.save(product);
+            return "{\"Product successfully created\":1}";
         }
     }
 
--- src/main/java/com/supinfo/proj/retailr/apistore/data/repository/ProductRepository.java
+++ src/main/java/com/supinfo/proj/retailr/apistore/data/repository/ProductRepository.java
@@ -2,7 +2,13 @@ package com.supinfo.proj.retailr.apistore.data.repository;
 
 import com.supinfo.proj.retailr.apistore.data.entity.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.util.pattern.PathPatternRouteMatcher;
+
+import java.util.Optional;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
-    Iterable<Product> findProductByEan(String string);
+    Optional<Product> findProductByEan(String string);
+    Iterable<Product> findProductByDepartment(String string);
+    Iterable<Product> findProductByFamily(String string);
+    Iterable<Product> findProductBySubfamily(String string);
 }
